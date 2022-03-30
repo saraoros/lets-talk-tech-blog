7b0a2bbea54661f5552e41660f64aa52243be503
Sara Oros Gochez <saraorosgochez@Saras-MacBook-Air.local>
2022-03-30 00:13:51 -0700

    started initial folder/file layout

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const Sequelize = require("sequelize");
+
+require("dotenv").config();
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(PROCESS.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3232,
+    });
+
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3232;
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
