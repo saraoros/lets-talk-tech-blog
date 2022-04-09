f5a50113d2080fa0c1bf5c17ec295d103213c8a6
Sara Oros Gochez <saraorosgochez@Saras-MacBook-Air.local>
2022-04-08 22:52:02 -0700

    fixed handlebar errors and paths, dashboard page error appeared

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const express = require("express");
-const session = require("express-session");
-const exphbs = require("express-handlebars");
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
+
+const helpers = require("./utils/helpers");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+
+const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
@@ -19,18 +20,18 @@ const sess = {
   }),
 };
 
-const helpers = require("./utils/helpers");
-
-const hbs = exphbs.create({ helpers });
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
 app.use(session(sess));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require("./controllers/"));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
