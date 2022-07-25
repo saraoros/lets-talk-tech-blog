51bd4e9d26c9de754642fcc182b90c9abe96dfb3
Sara Oros <94159081+saraoros@users.noreply.github.com>
2022-07-24 21:52:26 -0700

    Fixed seeds error and added user-seed data for posts

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,16 @@
 const seedPosts = require("./post-seeds");
+const seedUsers = require("./user-seeds");
 const sequelize = require("../config/connection");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
+  console.log("--------------");
+  await seedUsers();
+  console.log("--------------");
 
   await seedPosts();
   console.log("--------------");
+
   process.exit(0);
 };
 
